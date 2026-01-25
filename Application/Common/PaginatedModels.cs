27ca4c05ebfe1685c378936422e66348c62274ef
Your Name <tsurtsumia.lukaa@gmail.com>
2026-01-25 22:17:31 +0400

    adding paging

---
+++ Application/Common/PaginatedModels.cs
@@ -0,0 +1,35 @@
+﻿namespace BlogPage.Application.Common;
+
+public class PaginatedResult<T>
+{
+    public List<T> Data { get; set; } = new();
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+    public bool HasPrevious => Page > 1;
+    public bool HasNext => Page < TotalPages;
+}
+
+public class PaginationParams
+{
+    private const int MaxPageSize = 100;
+    private int _pageSize = 20;
+
+    public int Page { get; set; } = 1;
+    
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+    }
+}
+
+public class PostFilterParams : PaginationParams
+{
+    public string? Search { get; set; }
+    public string? Tag { get; set; }
+    public int? AuthorId { get; set; }
+    public string? SortBy { get; set; } = "date";
+    public string? SortOrder { get; set; } = "desc";
+}
---
+++ Application/Common/QueryableExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BlogPage.Application.Common;
+
+public static class QueryableExtensions
+{
+    public static async Task<PaginatedResult<T>> ToPaginatedListAsync<T>(
+        this IQueryable<T> query,
+        int page,
+        int pageSize)
+    {
+        var totalCount = await query.CountAsync();
+        var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+        
+        var items = await query
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+
+        return new PaginatedResult<T>
+        {
+            Data = items,
+            Page = page,
+            PageSize = pageSize,
+            TotalCount = totalCount,
+            TotalPages = totalPages
+        };
+    }
+
+}
